b0c37e07bbdc4f16bd8bcbebb62166d627199064
Anurag sharma <75670455+MR-Xcloud@users.noreply.github.com>
2024-11-27 11:58:12 +0530

    Update urls.py

--- smily/urls.py
+++ smily/urls.py
@@ -3,9 +3,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from smily import views
+from .views import imageapi
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("image",views.imageapi.as_view()),
-    # path("home",views.home)
+    # path("image",views.imageapi.as_view()),
+    # # path("upload",views.upload_view()),s
+    # path('upload/',views.upload_view, name='upload_file'),
+    # # path("home",views.home)
+    # path('upload/', upload, name='upload_view'),
+    path('image/', imageapi.as_view(), name='image_process'),
+    # path('upload/',views.upload_view),
 ]
+
